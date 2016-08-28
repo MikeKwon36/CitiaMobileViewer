e5e33b7e42944ffddf159c6fd96bfe27300b4290
MikeKwon36 <Mikeykwon@gmail.com>
2016-08-28 11:25:47 -0400

    Edits

--- app/src/main/java/com/kwondeveloper/citiamobileviewer/MainActivity.java
+++ app/src/main/java/com/kwondeveloper/citiamobileviewer/MainActivity.java
@@ -1,28 +1,18 @@
 package com.kwondeveloper.citiamobileviewer;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-
-import com.firebase.client.Firebase;
 
 import java.util.ArrayList;
 
